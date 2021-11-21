1b86ca301823ead7837cc9c2e2e09afccdbcf32c
Nazar Polyukhovich <nazar.polyukhovich@gmail.com>
2021-11-21 16:57:04 +0300

    edited login component

--- src/components/Header/Login/index.jsx
+++ src/components/Header/Login/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Login = () => {
     return (
         <div>
-            <p>Nazar Poliyulhoich</p>
+            <p>Nazar Polyukhovich</p>
         </div>
     );
 };
